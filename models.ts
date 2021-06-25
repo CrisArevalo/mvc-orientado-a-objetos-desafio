95f0890b9714efa37e6c332c07c0996844384ecf
CrisArevalo <cris.are.3496@gmail.com>
2021-06-25 19:08:21 -0300

    Model.ts completado

--- models.ts
+++ models.ts
@@ -1,7 +1,63 @@
+import * as contacts from "./contacts.json";
+const fs = require('fs');
 class Contact {
   id: number;
   name: string;
+  constructor(id:number,name:string){
+    this.id = id;
+    this.name = name;
+  }
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  array: any[] = [];
+  constructor(){
+
+  }
+
+  load(){ //Meto los datos del JSON al array.
+    contacts.forEach(t=> { //"contacts" es del import.
+      this.array.push(t);
+    })
+  } // FUNCIONA //
+
+  addOne(Contact:Contact){
+    const contacto = {
+      id:Contact.id,
+      name:Contact.name
+    }
+    this.array.push(contacto);
+  } // FUNCIONA //
+
+  getAll(){
+    return this.array;
+  } // FUNCIONA //
+
+  getOneById(id:number){
+    const filtrado = this.array.filter(t => {
+      return t.id == id;
+    }) 
+
+    return filtrado;
+  } // FUNCIONA //
+
+  save(){
+    fs.writeFile("contacts.json", JSON.stringify(this.array),"utf8", (err) => {
+      if(err) throw err;
+      console.log("Data has been saved!");      
+    });
+  } // FUNCIONA //
+}
+function main(){
+  const con = new ContactsCollection();
+  const persona = new Contact(5,"Cris");
+
+  con.load();
+  con.addOne(persona);
+  con.save();
+
+  
+}
+main();
+
+//export { ContactsCollection };
